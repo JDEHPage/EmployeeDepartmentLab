227050467d38358bebbecf93c123d690bd6cead2
John Page <jdehpage@gmail.com>
2019-04-02 09:40:36 +0100

    Lab start

---
+++ src/main/java/com/codeclan/example/EmployeeDepartmentLab/EmployeeDepartmentLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.EmployeeDepartmentLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDepartmentLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDepartmentLabApplication.class, args);
+	}
+
+}
